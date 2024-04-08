f94e1b0e622d21944565e0df8098a7089b6ad2d0
Petrica <41289735+busilapetrica@users.noreply.github.com>
2024-04-08 19:51:48 +0300

    add challenge from 04IntellijBassics

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -10,3 +10,7 @@ _
 /src/main/java/c02ProgrammingToolSetup/notes.txt,1\7\178ad9c1a6966d05e3b8d19a7bda0ce94fd9bc0d
 `
 0src/main/java/org/example/c03FirstStep/notes.txt,1\9\1901bd43999ef3c48e56d63a13bbd92f988cd5da
+e
+5src/main/java/org/example/s04IntellijBasics/notes.txt,e\8\e8159a43d0355d01367f9b3a57b74f4144e02b9f
+j
+:src/main/java/org/example/s04IntellijBasics/Challenge.java,a\e\ae8a89eb2f31b90334acee66a86a6c1b934c6538
---
+++ src/main/java/org/example/s04IntellijBasics/Challenge.java
@@ -0,0 +1,16 @@
+package org.example.s04IntellijBasics;
+
+public class Challenge {
+    public static void main(String[] args) {
+        double firstNumber = 20.00;
+        double secondNumber = 80.00;
+        double totalValue  = (firstNumber + secondNumber) * 100.00;
+        boolean checkNumber;
+        if(totalValue % 40.00 == 0){
+            checkNumber = true;
+        }else{
+            checkNumber = false;
+        }
+        System.out.println(checkNumber);
+    }
+}
